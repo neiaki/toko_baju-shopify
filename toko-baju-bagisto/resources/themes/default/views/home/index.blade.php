709c9c51f4daaa05801412f2833604c52dc035ad
NEki <febieki562@gmail.com>
2026-06-22 08:09:51 +0700

    Update dependencies and homepage view for deployment

--- toko-baju-bagisto/resources/themes/default/views/home/index.blade.php
+++ toko-baju-bagisto/resources/themes/default/views/home/index.blade.php
@@ -102,8 +102,8 @@
                         "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?w=400&q=80",
                         "https://images.unsplash.com/photo-1529139574466-a303027c1d8b?w=400&q=80",
                         "https://images.unsplash.com/photo-1483985988355-763728e1935b?w=400&q=80",
-                        "https://images.unsplash.com/photo-1539109136881-3be0616acf4b?w=400&q=80",
-                        "https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?w=400&q=80"
+                        "https://images.unsplash.com/photo-1496747611176-843222e1e57c?w=400&q=80",
+                        "https://images.unsplash.com/photo-1509631179647-0177331693ae?w=400&q=80"
                     ];
                 @endphp
                 @foreach($igPhotos as $src)
